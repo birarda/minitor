db8833d800b38deb2fd189fd987cd9bd02624ba7
Stephen Birarda <commit@birarda.com>
2014-01-29 10:46:12 -0800

    use NSApplication subclass to handle copy paste in prefs window

---
+++ Minitor/MinitorApplication.h
@@ -0,0 +1,13 @@
+//
+//  MinitorApplication.h
+//  Minitor
+//
+//  Created by Stephen Birarda on 1/29/2014.
+//  Copyright (c) 2014 Stephen Birarda. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MinitorApplication : NSApplication
+
+@end
